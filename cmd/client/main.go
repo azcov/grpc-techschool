f2c7820ac7820677f8a78a2dcba8fd0810a47c5e
Muhammad Mufid Aziz <mufidcode@gmail.com>
2021-06-29 10:39:29 +0700

    learn(eps-11): server-streaming

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"io"
 	"log"
 	"time"
 )
@@ -19,24 +20,76 @@ func main() {
 
 	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("cannot dial server %s: %v", *serverAddress, err)
+		log.Fatal("cannot dial server: ", err)
 	}
-	 laptopClient := pb.NewLaptopServiceClient(conn)
-	 laptop :=  sample.NewLaptop()
-	req := &pb.CreateLaptopRequest{Laptop: laptop}
 
-	 // set timeout
-	 ctx, cancel := context.WithTimeout(context.Background(),time.Second* 5)
+	laptopClient := pb.NewLaptopServiceClient(conn)
+	for i := 0; i < 10; i++ {
+		createLaptop(laptopClient)
+	}
+
+	filter := &pb.Filter{
+		MaxPriceUsd: 3000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.5,
+		MinRam:      &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
+	}
+
+	searchLaptop(laptopClient, filter)
+}
+
+func createLaptop(laptopClient pb.LaptopServiceClient) {
+	laptop := sample.NewLaptop()
+	laptop.Id = ""
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+	// set timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := laptopClient.CreateLaptop(ctx, req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			// not a big deal
+			log.Print("laptop already exists")
+		} else {
+			log.Fatal("cannot create laptop: ", err)
+		}
+		return
+	}
+
+	log.Printf("created laptop with id: %s", res.Id)
+}
+
+func searchLaptop(laptopClient pb.LaptopServiceClient, filter *pb.Filter) {
+	log.Print("search filter: ", filter)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	 res, err := laptopClient.CreateLaptop(ctx, req)
-	 if err != nil {
-	 	st, ok := status.FromError(err)
-	 	if ok && st.Code() == codes.AlreadyExists {
-	 		log.Fatal("laptop already exists")
-		}
-		 log.Fatal("cannot create laptop", err)
-	 }
+	req := &pb.SearchLaptopRequest{Filter: filter}
+	stream, err := laptopClient.SearchLaptop(ctx, req)
+	if err != nil {
+		log.Fatal("cannot search laptop: ", err)
+	}
 
-	 log.Printf("cerated laptop with id %s", res.Id)
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatal("cannot receive response: ", err)
+		}
+
+		laptop := res.GetLaptop()
+		log.Print("- found: ", laptop.GetId())
+		log.Print("  + brand: ", laptop.GetBrand())
+		log.Print("  + name: ", laptop.GetName())
+		log.Print("  + cpu cores: ", laptop.GetCpu().GetNumberCores())
+		log.Print("  + cpu min ghz: ", laptop.GetCpu().GetMinGhz())
+		log.Print("  + ram: ", laptop.GetRam())
+		log.Print("  + price: ", laptop.GetPriceUsd())
+	}
 }
